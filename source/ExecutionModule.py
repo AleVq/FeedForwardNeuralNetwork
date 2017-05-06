0665705fee5f40377a0bb4a8d82ea38e2d518c47
Ale <alexxandro.v2@gmail.com>
2017-05-06 17:32:27 +0200

    Feature selector completed

--- source/ExecutionModule.py
+++ source/ExecutionModule.py
@@ -1,13 +1,9 @@
-from parser import Parser
 from featureSelector import Selector
-import numpy as np
 
 def executeProgram():
-    parser = Parser('../risk_factors_cervical_cancer.csv')
-    selector = Selector(parser.dataSet)
-    R_1 = Selector.PearsonCoefficient(selector.dataSet, selector.x_mean, selector.y_mean, 0)
-    print(R_1)
-    return 0
+    selector = Selector('../risk_factors_cervical_cancer.csv')
+    featureSelectedDS = selector.apply_feature_selection(10)
+    print(featureSelectedDS)
 
 
 executeProgram()
--- source/featureSelector.py
+++ source/featureSelector.py
@@ -1,16 +1,68 @@
+import csv
 import numpy as np
 import math
 
 class Selector:
-    x_mean = 0
-    y_mean = 0
-    def __init__(self, matrix):
-        self.dataSet = np.array(matrix)
-        self.x_mean = np.nanmean(self.dataSet, axis=0)
-        self.y_mean = self.x_mean[self.x_mean.size-1]
-        self.x_mean = np.delete(self.x_mean, self.x_mean.size-1)
 
-    def PearsonCoefficient(ds, x_mean, y_mean, i):
+    def __init__(self, fileName):
+        self.dataSet = self.parse(fileName)
+        self.x_mean = np.nanmean(self.dataSet, axis=0)#each attribute's average value
+        self.y_mean = self.x_mean[self.x_mean.size-1]#target average value
+        self.x_mean = np.delete(self.x_mean, self.x_mean.size-1) #delete target column
+
+    def parse(self, filename):
+        temp = []
+        f = open(filename, 'r')
+        reader = csv.reader(f, delimiter = ';')
+        i = 0
+        for row in reader:
+            if i == 0:
+                self.attributes = row
+                i = 1
+            else:
+                temp.append(row)
+        temp = np.array(temp, dtype=float)
+        temp[temp==-1] = np.NaN
+        return temp
+
+    def apply_feature_selection(self, n): #numOfAttr: #arbitrarily choosing the n most relevant attributes w.r.t. the target
+        ranks = self.ranking_list(n)
+        return self.slicer(ranks)
+
+    def slicer(self, ranks): #delete not high-ranked attribute's column
+        orderedAttrNum = np.sort(ranks, axis=0)[:,0]
+        orderedAttrNum = orderedAttrNum.astype(int)
+        #slicing columns
+        k=0
+        j=0
+        for i in orderedAttrNum:#
+            if k == 0:
+                featureSelectedDS = np.column_stack((self.dataSet[:, i]))
+                k = 1
+            elif k == 1:
+                featureSelectedDS = np.column_stack((featureSelectedDS[0], self.dataSet[:, i]))
+                k = 2
+            else:
+                featureSelectedDS = np.column_stack((featureSelectedDS[:, 0:k], self.dataSet[:, i]))
+                k = k + 1
+        return featureSelectedDS
+
+    def ranking_list(self, n):
+        i = 0
+        score = np.zeros(shape=(self.dataSet.shape[1]-1,2))
+        while i < score.shape[0]: #loop through attributes except the target in the last column
+            score[i] = [i, math.fabs(self.pearson_coefficient(i))] #array of couple <score, i-th attribute>
+            i += 1
+        orderedScore =score[score[:,1].argsort(axis=0)] #in rank-ascending order
+        reversedScore = orderedScore[::-1]
+        return reversedScore[0:n]
+
+    def pearson_coefficient(self, i):
+        ds = self.dataSet
+        x_mean = self.x_mean
+        if x_mean[i] == 0:
+            return 0 #if in all instances this attribute is 0, for example
+        y_mean = self.y_mean
         k = 0
         sum_numerator = 0
         sum_denominator_1 = 0
